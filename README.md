7dcd95b69fd5e4bd9613bf1febd1862069ac39de
ivandi1980 <ivandi.djoh@gmail.com>
2020-04-26 02:41:52 +0700

    remove item from localstorage

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-## TodoList App
+## TodoList Application
 
-### About TodoList App
+### About TodoList Application
 
 This is a Small TodoList Application was created using Pure Vanilla Javascript.
 The purpose of this Application is to exercise my coding skill specially on Javascript basic things.
 This Application using localstorage to save the data.
 
-### Getting Started
+### Getting Started TodoList Application
 
 For the Installation Process, please follow these inctructions :
 
     1. Download as .zip or clone this repo using git clone https://github.com/ivandi1980/todolist.git.
-    2. Put it into your Webserver such as htdocs for XAMPP or you ca use live-server plugin in VSCode
+    2. Put it into your Webserver such as htdocs for XAMPP or you cas use live-server plugin in VSCode
 
-### Screenshoot
+### Screenshoot of TodoList Application
 
 For preview of this TodoList Application, here are the screenshoot :
 
-![Dashboard](captured/one.png "This is the Home's Captured")
-![Dashboard](captured/two.png "This is the Home's Captured")
+![Dashboard](captured/one.jpg "This is the Home's Captured")
+![Dashboard](captured/two.jpg "This is the Home's Captured")
--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todo");
 
 // Event Listener
+document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener("click", filterTodo);
@@ -23,6 +24,8 @@ function addTodo(event) {
   newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
+  // Add Todo into Localstorage
+  savelocalTodos(todoInput.value);
 
   // Check the mark Button
   const completedButton = document.createElement("button");
@@ -50,6 +53,7 @@ function deleteCheck(e) {
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
     todo.classList.add("fall");
+    removeLocalTodos(todo);
     todo.addEventListener("transitionend", function () {
       todo.remove();
     });
@@ -86,4 +90,73 @@ function filterTodo(e) {
         break;
     }
   });
+}
+
+// save the todolist data into localstorage
+function savelocalTodos(todo) {
+  // check if already have a data in localstorage
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  // if there's no data on localstorage
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// get todos from localstorage
+function getTodos() {
+  // check if already have a data in localstorage
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  // loop over todos
+  todos.forEach(function (todo) {
+    // Create todoDiv
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    // Create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todo;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    // Check the mark Button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    // Check the trash Button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    // Append to the List
+    todoList.appendChild(todoDiv);
+  });
+}
+
+// remove todos from the localstorage
+function removeLocalTodos(todo) {
+  // check if already have a data in localstorage
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
